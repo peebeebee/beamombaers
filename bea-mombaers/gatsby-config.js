d37d2688235827f6b9926698ba5b3d6c8208be85
Peter Briers <peter.briers@gmail.com>
2021-07-01 13:57:26 +0200

    Added siteUrl to config for gatsby-plugin-sitemap

--- bea-mombaers/gatsby-config.js
+++ bea-mombaers/gatsby-config.js
@@ -1,7 +1,7 @@
 module.exports = {
   siteMetadata: {
     title: "Bea Mombaers",
-    siteMetadata: 'https://beamombaers.com'
+    siteUrl: 'https://beamombaers.com'
   },
   plugins: [
     "gatsby-plugin-sass",
