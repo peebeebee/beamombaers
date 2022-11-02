80625b1d96c5fd7242146f2cc95a6cfd9125afee
Peter Briers <peter.briers@gmail.com>
2022-11-02 14:39:02 +0100

    Unknown api Gatsby-browser

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,14 +1,5 @@
 import "./src/styles/global.scss";
 
-
-// Logs when the client route changes
 export function onRouteUpdate() {
-  // TODO
   document.body.focus();
 }
-
-export function onRenderBody({ setBodyAttributes }){
-  setBodyAttributes({
-      tabIndex: '-1'
-    });
-};
