da3b1793ede3b7edee465fe9185fd573eacc3953
Peter Briers <peter.briers@gmail.com>
2026-06-22 20:02:23 +0200

    Header component

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -1,4 +1,6 @@
-export function onRenderBody({ setHtmlAttributes, setBodyAttributes }) {
+import React from "react";
+
+export function onRenderBody({ setHtmlAttributes, setBodyAttributes, setHeadComponents }) {
   setHtmlAttributes({
     lang: "en" 
   });
@@ -6,4 +8,8 @@ export function onRenderBody({ setHtmlAttributes, setBodyAttributes }) {
   setBodyAttributes({
       tabIndex: '-1'
   });
+
+  setHeadComponents([
+    <meta name="theme-color" content="#ffffff" key="theme-color" />
+  ]);
 };
